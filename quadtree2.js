9f9f734ddb51c0da0a39320c259f5885e0173684
Burning Gramma <burninggramma@gmail.com>
2014-01-24 17:19:15 +0100

    Adds metaprogramming tricks for guarded functions.

--- quadtree2.js
+++ quadtree2.js
@@ -1,18 +1,66 @@
+/*jshint loopfunc: true */
 var Vec2 = require('vec2'),
     Quadtree2;
 
 Quadtree2 = function Quadtree2(size, limit) {
-  if (!(size instanceof Vec2))
-    throw new Error('First arg should be an instance of Vec2');
+  var id,
 
-  if ('number' !== typeof limit)
-    throw new Error('Second arg should be a number');
+      // Container for private data.
+      data = {},
 
-  var data = {},
-      addObject = function addObject(){
+      // Functions will be exported without init check.
+      initFns = {
+        getLimit : function getLimit(){
+          return data.limit_;
+        },
+
+        setLimit : function setLimit(limit){
+          if (limit === undefined) return;
+
+          if ('number' !== typeof limit)
+            throw new Error('Parameter should be a number');
+
+          data.limit_ = limit;
+        },
+
+        getSize : function getSize(){
+          return data.size_.clone();
+        },
+
+        setSize : function setSize(size){
+          if (size === undefined) return;
+
+          if (!(size instanceof Vec2))
+            throw new Error('Parameter should be an instance of Vec2');
+
+          data.size_ = size.clone();
+        },
+
+        init : function init() {
+          // TODO should check every property is set.
+        }
+      },
+
+      // Functions will be exported together with init check.
+      runFns = {
+        addObject : function addObject(id){
+          // TODO implement me!
+        }
       };
 
-  this.addObject = addObject;
+  initFns.setSize(size);
+  initFns.setLimit(limit);
+
+  for (id in initFns) {
+    this[id] = initFns[id];
+  }
+
+  for (id in runFns) {
+    this[id] = function() {
+      if (!data.inited_) this.init();
+      runFns[id].apply(this, arguments);
+    };
+  }
 };
 
 module.exports = Quadtree2;
--- test/test.js
+++ test/test.js
@@ -1,19 +1,38 @@
 var Quadtree2 = require('../quadtree2'),
+    Vec2      = require('vec2'),
     assert    = require('assert'),
-    should    = require('should');
+    should    = require('should'),
+    factory;
+
+factory = function factory(){
+  return new Quadtree2(new Vec2(2,3), 4);
+};
 
 describe('Quadtree2', function(){
   describe('constructor', function(){
-    it('should throw without proper arguments', function(){
+    it('should throw with inproper arguments', function(){
       (function() { Quadtree2(2,3); }).should.throw();
     });
 
     it('should not throw with proper arguments', function(){
-      (function() { Quadtree2(new Vec2(2,3), 4); }).should.throw();
+      (function() { Quadtree2(new Vec2(2,3), 4); }).should.not.throw();
     });
 
     it('sets width and height if they are passed', function() {
+      var qt = factory();
+      qt.addObject(1);
+    });
+  });
+
+  describe('#getSize', function(){
+    it('returns just a clone of the size', function() {
+      var qt    = factory(),
+          size  = qt.getSize();
+
+      size.x = 5;
+
+      qt.getSize().x.should.not.eql(5);
+      qt.getSize().x.should.eql(2);
     });
   });
 });
-
