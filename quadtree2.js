6e910010fb0dda0a66a9e4a97c4adbda895eea42
Burning Gramma <burninggramma@gmail.com>
2014-01-25 11:23:19 +0100

    Adds metaprogrammed validation and cleans a bit.

--- quadtree2.js
+++ quadtree2.js
@@ -9,7 +9,7 @@ var injector = function injector(cbBrowser, cbNodeJS){
 
     // Requiring the Vec2 class
     Vec2 =  injector(function() {
-              if (!window.Vec2) throw new Error('Vec2 is a requirement');
+              if (!window.Vec2) { throw new Error('Vec2 is a requirement'); }
               return window.Vec2;
             },
             function(){
@@ -22,43 +22,129 @@ var injector = function injector(cbBrowser, cbNodeJS){
 Quadtree2 = function Quadtree2(size, limit) {
   var id,
 
-      // Container for private data.
-      data = { map_ : {}, count_ : 0 },
-
-      // Property definitions
-      constraints = {
-        data : {
-          necessary : ['size', 'limit']
-        }
+      // Inserted object keys.
+      k = {
+        p  : 'pos_',
+        r  : 'rad_',
+        R  : 'rot_',
+        id : false
       },
 
-      // Private function definitions
-      fns = {
-        init : function init() {
-          var i;
-
-          for(i in constraints.data.necessary) {
-            if (data[constraints.data.necessary[i] + '_'] === undefined)
-              throw new Error('Can not work without the necessary properties');
+      // Validation definitions
+      validate = {
+        isNumber : function isNumber(param) {
+          if ('number' !== typeof param) {
+            throw new Error('NaN - not a Number');
           }
         },
 
-        checkInit : function checkInit() {
-          if (!data.inited_) fns.init();
+        isString : function isString(param) {
+          if (typeof val !== 'string') {
+            throw new Error('NaS - not a String');
+          }
+        },
+
+        isVec2 : function isVec2(param) {
+          if ('object' !== typeof param || !(param instanceof Vec2)) {
+            throw new Error('NaV - not a Vec2');
+          }
+        },
+
+        isDefined : function isDefined(param) {
+          if (param === undefined) {
+            throw new Error('ND - not defined');
+          }
+        },
+
+        isObject : function isObject(param) {
+          if ('object' !== typeof param) {
+            throw new Error('NaO - not an Object');
+          }
+        },
+
+        hasKey : function hasKey(obj, key) {
+          if ( Object.keys(obj).indexOf(key) === -1 ) {
+            throw new Error('HNK - has no key: ' + key);
+          }
+        },
+
+        byCallbackObject : function byCallbackObject(obj, cbObj, keyTable) {
+          var key;
+          for (key in cbObj) {
+            if (keyTable !== undefined) {
+              cbObj[key](obj[keyTable[key]]);
+            } else {
+              cbObj[key](obj[key]);
+            }
+          }
+        }
+      },
+
+      // Container for private data.
+      data = { map_ : {}, count_ : 0, ids_ : 0, meta_ : { shapes : {} } },
+
+      // Property definitions.
+      constraints = {
+        data : {
+          necessary : {
+            size_  : validate.isVec2,
+            limit_ : validate.isNumber
+          }
+        },
+
+        k : {
+          necessary : {
+            p : validate.isVec2
+          },
+
+          circle : {
+            necessary : {
+              r : validate.isNumber
+            },
+          },
+
+          rect : {
+            necessary : {
+              R : validate.isNumber
+            },
+          }
+        }
+      },
+
+      // Private function definitions.
+      fns = {
+        nextId : function nextId() {
+          return ++data.ids_;
+        },
+
+        init : function init() {
+          validate.byCallbackObject(data, constraints.data.necessary);
+        },
+
+        checkInit : function checkInit(init) {
+          if (init && !data.inited_) { fns.init(); }
+          return data.inited_;
+        },
+
+        checkObjectKeys : function checkObjectKeys(obj) {
+          validate.isDefined(obj);
+          validate.isObject(obj);
+          validate.byCallbackObject(obj, constraints.k.necessary, k);
+
+          if (k.id) { validate.isNumber(obj[k.id]); }
         }
       },
 
       // Public function definitions
       publicFns = {
-        getLimit : function getLimit(){
+        getLimit : function getLimit() {
           return data.limit_;
         },
 
-        setLimit : function setLimit(limit){
-          if (limit === undefined) return;
+        setLimit : function setLimit(limit) {
+          if (limit === undefined) { return; }
 
-          if ('number' !== typeof limit)
-            throw new Error('Parameter should be a number');
+          validate.isNumber(limit);
 
           data.limit_ = limit;
         },
@@ -67,27 +153,29 @@ Quadtree2 = function Quadtree2(size, limit) {
           return data.count_;
         },
 
-        getSize : function getSize(){
+        setObjectKey : function setObjectKey(key, val) {
+          validate.hasKey(k, key);
+          validate.isString(k, val);
+
+          k[key] = val;
+        },
+
+        getSize : function getSize() {
           return data.size_.clone();
         },
 
-        setSize : function setSize(size){
-          if (size === undefined) return;
+        setSize : function setSize(size) {
+          if (size === undefined) { return; }
 
-          if('object' !== typeof size || !(size instanceof Vec2))
-            throw new Error('Parameter should be an instance of Vec2');
+          validate.isVec2(size);
 
           data.size_ = size.clone();
         },
 
-        addRect : function addRect(id, origin, size, rotation) {
-          fns.checkInit();
+        addObject : function addObject(obj) {
+          fns.checkInit(true);
+          fns.checkObjectKeys(obj);
           data.count_++;
-        },
-
-        addCircle : function addCircle(id, origin, radius) {
-          fns.checkInit();
-          throw new Error('Not implemented');
         }
       };
 
--- test/test.js
+++ test/test.js
@@ -16,24 +16,24 @@ factory = function factory(size, limit){
 describe('Quadtree2', function(){
   describe('constructor', function(){
     context('with inproper arguments', function() {
-      it('should throw', function(){
-        (function() { factory(1,2); }).should.throw();
+      it('should throw NaV', function(){
+        factory.bind(null, 1,2).should.throw(/^NaV/);
       });
 
-      it('should throw', function(){
-        (function() { factory(new Vec(2,3), null); }).should.throw();
+      it('should throw NaN', function(){
+        factory.bind(null, new Vec2(2,3), null).should.throw(/^NaN/);
       });
     });
 
     context('with proper arguments', function() {
       it('should not throw', function(){
-        (function() { factory(new Vec2(2,3), 4); }).should.not.throw();
+        factory.bind(null, new Vec2(2,3), 4).should.not.throw();
       });
     });
 
     context('without limit', function() {
       it('should not throw', function() {
-        (function() { factory(new Vec2(2,3), undefined); }).should.not.throw();
+        factory.bind(null, new Vec2(2,3), undefined).should.not.throw();
       });
     });
   });
@@ -50,12 +50,25 @@ describe('Quadtree2', function(){
     });
   });
 
-  describe('#addRect', function(){
+  describe('#addObject', function(){
+    context('with inproper arguments', function() {
+      it('should throw ND', function() {
+        var qt = factory();
+        qt.addObject.should.throw(/^ND/);
+      });
+
+      it('should throw ', function() {
+        var qt = factory();
+        qt.addObject.bind(null, 1).should.throw(/^NaO/);
+      });
+    });
+
     it('should increase the object count', function() {
-      var qt = factory();
+      var qt = factory(),
+          o  = { pos_ : new Vec2(1,2) };
 
       qt.getCount().should.eql(0);
-      qt.addRect();
+      qt.addObject(o);
       qt.getCount().should.eql(1);
     });
   });
